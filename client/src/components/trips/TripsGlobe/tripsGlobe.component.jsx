7872fc908b9c2652843d80da305f6a7e0087aa07
reginafernandezpinar <regina.fernandez.p@gmail.com>
2020-08-01 12:40:11 +0200

    update dependencies

--- client/src/components/trips/TripsGlobe/tripsGlobe.component.jsx
+++ client/src/components/trips/TripsGlobe/tripsGlobe.component.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { toastr } from "react-redux-toastr";
-import Cesium from "cesium";
+import Cesium from 'cesium';
 
 // Import global resources
 
